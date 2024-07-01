a80a241353d0e2f37332f8a0d2b91d58fece62d1
theprajwalkumar <prajwalprakash20@gmail.com>
2024-07-01 15:18:47 +0530

    thirdpushloops

---
+++ src/Loop/Pattern.java
@@ -0,0 +1,30 @@
+package Loop;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		for(int x=0;x<=5;x++) {
+			for(int y=0;y<=x;y++) {
+				
+				System.out.print("*");
+				
+				
+				
+				
+			}
+			System.out.println();
+		}
+
+	
+	for(int z=0;z<=5;z++) {
+		for (int k=5;k>=z;k--) {
+			System.out.print("*");
+			
+		}
+		System.out.println();
+	}
+		
+	}
+}
+
+
---
+++ src/Loop/Repractice.java
@@ -0,0 +1,21 @@
+package Loop;
+
+public class Repractice {
+
+	public static void main(String[] args) {
+		for(int x=0;x<=5;x++) {
+			for(int y=0;y<=x;y++) {
+				System.out.print(y);
+			}
+			System.out.println();
+		}
+		for(int z=0;z<=5;z++) {
+			for (int k=5;z<=k;k--) {
+				System.out.print(k);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
