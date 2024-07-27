865854fafdef67f5f634e31012795f2a08be2f32
theprajwalkumar <prajwalprakash20@gmail.com>
2024-07-27 13:44:22 +0530

    arraysort,string reverse

---
+++ src/ArraySort/Arraysort.java
@@ -0,0 +1,18 @@
+package ArraySort;
+
+import java.util.Arrays;
+
+public class Arraysort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[]cars= {"Aprilla","Audi","TVS","Ferari","Bentley"};
+		Arrays.sort(cars);
+		for(String i: cars) {
+			System.out.println(i);
+		}
+	
+
+	}
+
+}
---
+++ src/Stringreverse/StringRev.java
@@ -0,0 +1,23 @@
+package Stringreverse;
+
+public class StringRev {
+	
+	  
+		public static String reverseString(String str){  
+		    StringBuilder sb=new StringBuilder(str);  
+		    sb.reverse();  
+		    return sb.toString();  
+		}  
+		
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(StringRev.reverseString("My name is Shahrukh"));
+		System.out.println(StringRev.reverseString("My name is Prajwal"));
+		
+		 System.out.println(StringRev.reverseString("my name is khan"));  
+		    System.out.println(StringRev.reverseString("I am sonoo jaiswal"));  
+
+	}
+
+}
